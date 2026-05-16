53cf20d052050bc33418f249c3ca72768019ae47
The One Who Overcame Adversity <skythelight15@gmail.com>
2026-05-16 14:14:28 +0800

    start up

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is the homepage customized by [Your Name].</p>
+    </div>
+  );
+}
